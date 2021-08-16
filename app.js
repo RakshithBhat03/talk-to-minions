72affeb2ede626b89fabda4fdc411dd0982c352f
RakshithBhat03 <rakshith6666@gmail.com>
2021-08-16 21:32:21 +0530

    add fucntionalites

---
+++ app.js
@@ -0,0 +1,23 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var inputText = document.querySelector("#userInput");
+var outputText = document.querySelector("#translatedOutput");
+var url = "https://api.funtranslations.com/translate/minion.json";
+btnTranslate.addEventListener("click", clickHandler);
+function clickHandler(event) {
+  var message = inputText.value;
+  var finalUrl = urlHandler(message);
+  console.log(finalUrl);
+  fetch(finalUrl)
+    .then((response) => response.json())
+    .then((json) => {
+      outputText.innerText = json.contents.translated;
+    })
+    .catch(() => {
+      {
+        alert("Error occured");
+      }
+    });
+}
+function urlHandler(text) {
+  return url + "?text=" + text;
+}
